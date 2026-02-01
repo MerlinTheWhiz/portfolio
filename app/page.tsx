c4b24764b0f3ec66d930be014af775efa40b3932
MerlinTheWhiz <michaelnathan505@gmail.com>
2026-02-02 00:00:30 +0100

    Experience section with mock data

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/sections/Hero";
 import SkillsList from "@/components/sections/Skills";
 import Projects from "@/components/sections/Projects";
 import Experience from "@/components/sections/Experience";
-import Contact from "@/components/sections/Contact";
+// import Contact from "@/components/sections/Contact";
 
 export default function Home() {
   return (
