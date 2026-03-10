ba98de84a7bce2fec4dc1c0e497c25e70339d54d
MerlinTheWhiz <michaelnathan505@gmail.com>
2026-03-11 00:27:47 +0100

    chore: projects page first commit

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,68 @@
+import Image from "next/image";
+import { projects } from "@/lib/projects";
+import { ArrowUpRight } from "lucide-react";
+import { LuGithub } from "react-icons/lu";
+
 export default function Projects() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Projects Page
-        </h1>
-      </main>
-    </div>
+    <main className="flex flex-col relative bg-background py-20 sm:py-30 justify-center items-center px-16 sm:items-start">
+      <div className="grid lg:grid-cols-2 gap-8 mb-16 max-w-6xl mx-auto">
+        {projects
+          .filter((p) => p.featured)
+          .map((project) => (
+            <div
+              key={project.id}
+              className="group relative rounded-3xl overflow-hidden border transition-all duration-500
+                         bg-background-card border-border-default hover:border-accent-primary/40 
+                          dark:hover:border-accent-primary/40 p-6"
+            >
+              <div className="aspect-10/5 overflow-hidden rounded-2xl mb-6">
+                <Image
+                  src={project.image}
+                  alt={project.title}
+                  className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+              </div>
+
+              <div className="">
+                <div className="flex flex-wrap gap-2 mb-4">
+                  {project.tags.map((tag) => (
+                    <span
+                      key={tag}
+                      className="px-3 py-1 rounded-full text-xs bg-[#dbeafe] dark:bg-white/5 text-text-secondary"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+                <h3 className="text-2xl font-bold mb-2 text-text-primary group-hover:text-accent-primary transition-colors duration-300">
+                  {project.title}
+                </h3>
+                <p className="line-clamp-2 mb-6 text-text-muted">
+                  {project.description}
+                </p>
+                <div className="flex items-center gap-4">
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline-flex items-center gap-2 text-sm font-medium text-accent-primary hover:text-accent-hover transition-colors"
+                  >
+                    View Project <ArrowUpRight size={16} />
+                  </a>
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="p-2 rounded-full transition-colors bg-[#dbeafe] dark:bg-white/5 hover:bg-[#cfe5ff] dark:hover:bg-white/10"
+                  >
+                    <LuGithub size={18} className="text-text-muted" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </main>
   );
 }
